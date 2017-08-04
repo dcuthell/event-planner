8b5b8006eb26ea30a81fcf38dc3df35829ab8947
dcuthell <dcc1447@gmail.com>
2017-08-04 12:49:38 -0700

    add basic UI for app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,33 @@
 import models.EventPlanner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         EventPlanner newEventPlanner = new EventPlanner();
+
+        System.out.println("Welcome to the Event Planner!");
+
+        boolean programRunning = true;
+
+        while(programRunning){
+            Integer userInput;
+            System.out.println("Please Select from the Following Menu Items:\n1  -  Create a New Event\n0  -  Exit");
+            try{
+                userInput = Integer.parseInt(bufferedReader.readLine());
+                if(userInput.equals(1)){
+                    System.out.println("Create a New Event");
+                }else if(userInput.equals(0)){
+                    System.out.println("Goodbye!");
+                    programRunning = false;
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
