8d2a6c7b2496dd4c65053d51a2bb181ffe50fb90
dcuthell <dcc1447@gmail.com>
2017-08-04 12:30:47 -0700

    add EventPlanner class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,8 @@
-import models.Event;
+import models.EventPlanner;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        Event newEvent = new Event();
-        System.out.println(newEvent.getPartySizeMultiplier(1));
+        EventPlanner newEventPlanner = new EventPlanner();
     }
 }
---
+++ src/main/java/models/EventPlanner.java
@@ -0,0 +1,14 @@
+package models;
+
+import java.util.ArrayList;
+
+public class EventPlanner {
+    ArrayList<Event> allEvents = new ArrayList<>();
+    Event newEvent;
+
+    public EventPlanner(){
+        newEvent = new Event();
+    }
+
+
+}
